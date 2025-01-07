68a37592839fe8319f2863715834c8707d65baee
MJohnson <48001236+screencancer@users.noreply.github.com>
2025-01-07 18:28:42 -0500

    api ver and key support

--- app/main.py
+++ app/main.py
@@ -18,17 +18,30 @@ def main():
     
     server.close
 
-def message_init(msg, correlation_id):
+def message_init(msg, correlation_id, api_ver, api_key, error_code):
+    
     msg_size = msg.to_bytes(4, byteorder="big", signed=True)
     cid = correlation_id.to_bytes(4, byteorder="big")
+    error_code = error_code.to_bytes(4, byteorder="big")
+    #key = api_key.to_bytes(4, byteorder="big")
+
+    
     #To get find offset
     #corr_id = correlation_id.to_bytes(4, byteorder="big", signed=True)
-    return(msg_size+cid)
+    return(msg_size+cid + error_code)
 
 def client_handling(client):
     request = client.recv(1024)
-    response = int.from_bytes(request[8:12], byteorder="big")
-    client.sendall(message_init(0,response))
+
+    cid_bytes = int.from_bytes(request[8:12], byteorder="big")
+    api_ver = int.from_bytes(request[5:8], byteorder="big")
+    api_key = int.from_bytes(request[3:5])
+    error_code = 0
+    if api_ver not in [range(5)]:
+        api_ver=4
+        error_code = 35
+
+    client.sendall(message_init(0,cid_bytes, api_ver, api_key, error_code))
     client.close
 
 
